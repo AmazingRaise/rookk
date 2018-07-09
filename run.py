d59cc8e717df8cb1eea50c756baa9b8f9ac8d4a0
zhangmianze <zhangmianze@uusense.com>
2018-07-09 22:09:54 +0800

    添加启动

---
+++ run.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+# @Time    : 2018/7/9 下午10:08
+# @Author  : Aries
+# @Site    : 
+# @File    : run.py.py
+# @Software: PyCharm Community Edition
+from rookk_main import app
+
+
+if __name__ == '__main__':
+    app.run()
